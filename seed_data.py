a875d7f45f6ff41e58b0fced73c9c456232262fe
Patricio Noce <patricionoce@gmail.com>
2022-10-14 21:32:52 -0300

    cambio de nombres

--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,9 @@
 from ejemplo.models import Familiar
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(nombre="Patricio", direccion="Manola Castre 1473", numero_pasaporte=33444888).save()
+Familiar(nombre="Cintia", direccion="Manola Castre 1473", numero_pasaporte=30500228).save()
+Familiar(nombre="Gonzalo", direccion="Dardo Roche 3480", numero_pasaporte=345345).save()
+Familiar(nombre="Esteban", direccion="Dardo Roche 3480", numero_pasaporte=567567).save()
+Familiar(nombre="Rosalia", direccion="Calle Falsa 123", numero_pasaporte=5987400).save()
 
 print("Se cargo con éxito los usuarios de pruebas")
