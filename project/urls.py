13ff31d7b2d8a221cf45c1744713ce035333e570
Patricio Noce <patricionoce@gmail.com>
2022-10-18 23:23:34 -0300

    Se agrego class Base Views. Forms y Herencia de Templates

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import imc, index, index_dos, index_tres, monstrar_familiares
+from ejemplo.views import (imc, index, index_dos, index_tres, monstrar_familiares, BuscarFamiliar)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('mostrar-notas/', index_tres),
     path('calcular-imc/<peso>/<altura>', imc),
     path('mi-familia/', monstrar_familiares),
+    path('mi-familia/buscar', BuscarFamiliar.as_view()),
 ]
