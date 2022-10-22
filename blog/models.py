93cdd2372b19d9cc6866e52d5bfb2f900624bf48
Patricio Noce <patricionoce@gmail.com>
2022-10-22 00:23:27 -0300

    Se agrego Blog con admin y cambiamos titulos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from traitlets import default
 
-# Create your models here.
+class Configuracion(models.Model):
+    nombre_blog = models.CharField(max_length=20)
+    construido_por = models.CharField(max_length = 30)
+    titulo_principal = models.CharField(max_length = 30, default='')
+    subtitulo_principal = models.CharField(max_length = 30, default='')
+
